1e04d89d5cfdef36c8e668d4749fe2d833629eef
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-11 01:34:19 -0400

    Improved aesthetics of form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,22 @@
 @section('content')
     <h1>Grade Calculator</h1>
     <div class="container">
-        <form action="" method="post" >
-            <input type="text" name="course-item">
-            <input type="text" name="worth-percent">
-            <input type="text" name="mark-percent">
-            <button type="submit" >Add entry</button>
+        <form action="" method="post" class="form-group">
+            <div class="form-group">
+                <label for="course-item">Course Item</label>
+                <input type="text" name="course-item">
+            </div>
+            <div class="form-group">
+                <label for="worth-percent">Worth (in %)</label>
+                <input type="text" name="worth-percent">
+            </div>
+            <div class="form-group">
+                <label for="worth-percent">Your Mark (in %)</label>
+                <input type="text" name="mark-percent">
+            </div>
+            <button type="submit" class="btn btn-primary">Add Entry</button>
         </form>
-        
+
         <table class="table">
             <thead>
               <tr>

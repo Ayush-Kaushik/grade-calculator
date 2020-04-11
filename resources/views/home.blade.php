68339bd04483930716699ba3629cf718b70c91b2
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-11 02:52:27 -0400

    Fixing white label text issue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,16 @@
     <div class="container">
         <form action="" method="post" class="form-group">
             <div class="form-group">
-                <div class="label"><label for="course-item">Course Item</label></div>
-                <div class="input"><input type="text" name="course-item"></div>
+                <div class="form-label"><label for="course-item">Course Item</label></div>
+                <div class="form-input"><input type="text" name="course-item"></div>
             </div>
             <div class="form-group">
-                <div class="label"><label for="worth-percent">Worth (in %)</label></div>
-                <div class="input"><input type="text" name="worth-percent"></div>
+                <div class="form-label"><label for="worth-percent">Worth (in %)</label></div>
+                <div class="form-input"><input type="text" name="worth-percent"></div>
             </div>
             <div class="form-group">
-                <div class="label"><label for="worth-percent">Your Mark (in %)</label></div>
-                <div class="input"><input type="text" name="mark-percent"></div>
+                <div class="form-label"><label for="worth-percent">Your Mark (in %)</label></div>
+                <div class="form-input"><input type="text" name="mark-percent"></div>
             </div>
             <button type="submit" class="btn btn-primary">Add Entry</button>
             <button class="btn btn-warning">Clear Entry</button>

e069fb89bf269504aecaf0123771f8d11185ab86
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-11 01:29:57 -0400

    Added form for user course marks entry

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,13 @@
 @section('content')
     <h1>Grade Calculator</h1>
     <div class="container">
+        <form action="" method="post" >
+            <input type="text" name="course-item">
+            <input type="text" name="worth-percent">
+            <input type="text" name="mark-percent">
+            <button type="submit" >Add entry</button>
+        </form>
+        
         <table class="table">
             <thead>
               <tr>

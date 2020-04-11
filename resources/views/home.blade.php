6aaa3b7e986ba853c4818dfdbbe1941092ccf60d
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-11 01:18:03 -0400

    Added table layout for grade calculator

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.main')
 @section('content')
     <h1>Grade Calculator</h1>
-
+    <div class="container">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Item</th>
+                <th scope="col">Worth (in %)</th>
+                <th scope="col">Your Mark (in %)</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+    </div>
 @endsection

ce6576eb210446345e4ca34ad544dca08cfce6ee
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-13 22:22:05 -0400

    Added create endpoint

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class CalculatorController extends Controller

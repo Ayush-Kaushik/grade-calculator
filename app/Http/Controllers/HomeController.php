bf50a9f850c82d68a180f419c19c5ccba2c31854
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-13 22:14:28 -0400

    Saving course title in sessions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
 
     public function create(Request $request)
     {
-        $course_title = $request->course_name;
+        $request->session()->put('course_title', $request->course_name);
         return view('/calculator', compact('course_title'));
     }
 }

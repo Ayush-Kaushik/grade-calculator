79f2e9bfc30d61db3030d766be34043766637fa4
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-18 19:27:33 -0400

    Updated table design

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -1,7 +1,10 @@
 @extends('layout.main')
 @section('content')
-    <h1>Course title: {{Session::get('course_title')}}</h1>
-    <div class="container">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h1 class="m-0 font-weight-bold text-primary">{{Session::get('course_title')}}</h1>
+        </div>
+
         <form action="{{ action('CalculatorController@create') }}" method="post" class="form-group">
             {{ csrf_field() }}
             <div class="form-group">
@@ -28,48 +31,53 @@
                 </ul>
             </div>
         @endif
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Item</th>
-                <th scope="col">Worth (in %)</th>
-                <th scope="col">Your Mark (in %)</th>
-              </tr>
-            </thead>
-            <tbody>
-                @if(Session::has('marks_list'))
-                    @foreach (Session::get('marks_list') as $itemKey => $itemValue)
-                    <tr>
-                        <td>{{$itemValue['course_item']}}</td>
-                        <td>{{$itemValue['worth_percent']}}</td>
-                        <td>{{$itemValue['mark_percent']}}</td>  
-                        <td>
-                            <form action="{{ action('CalculatorController@destroy', $itemKey) }}" method="post">
-                                {{ method_field('DELETE') }}
-                                {{ csrf_field() }}
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>      
-                    </tr>
-                    @endforeach
-                @endif
-            </tbody>
-          </table>
+        
+        <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" width="100%" cellspacing="0">
+                <thead>
+                <tr>
+                    <th scope="col">Item</th>
+                    <th scope="col">Worth (in %)</th>
+                    <th scope="col">Your Mark (in %)</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @if(Session::has('marks_list'))
+                        @foreach (Session::get('marks_list') as $itemKey => $itemValue)
+                        <tr>
+                            <td>{{$itemValue['course_item']}}</td>
+                            <td>{{$itemValue['worth_percent']}}</td>
+                            <td>{{$itemValue['mark_percent']}}</td>  
+                            <td>
+                                <form action="{{ action('CalculatorController@destroy', $itemKey) }}" method="post">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>      
+                        </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
 
-          @if(Session::has('current_mark'))
-            <p>Your current Mark is {{Session::get('current_mark')}}%</p>
-          @endif
+            @if(Session::has('current_mark'))
+                <p>Your current Mark is {{Session::get('current_mark')}}%</p>
+            @endif
 
-          @if(Session::has('final_exam_worth'))
-            <p>Final exam worth {{Session::get('final_exam_worth')}}%</p>
-          @endif
+            @if(Session::has('final_exam_worth'))
+                <p>Final exam worth {{Session::get('final_exam_worth')}}%</p>
+            @endif
 
-          @if(Session::has('final_exam_stats'))
-            @foreach (Session::get('final_exam_stats') as $itemKey => $itemValue)
-                <p>To finish with {{$itemKey}} required on final: {{$itemValue}}%</p>
-            @endforeach
-          @endif
-        <div>
+            @if(Session::has('final_exam_stats'))
+                @foreach (Session::get('final_exam_stats') as $itemKey => $itemValue)
+                    <p>To finish with {{$itemKey}} required on final: {{$itemValue}}%</p>
+                @endforeach
+            @endif
+            <div>
+            </div>
+        </div>
         </div>
     </div>
 @endsection

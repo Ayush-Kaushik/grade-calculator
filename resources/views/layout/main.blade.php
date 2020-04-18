5deeba27414b3b25f4f3b7b3f8dee82cba1b637c
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-18 15:50:54 -0400

    Working with sb-admin theme bootstrap

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,21 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Grade Calculator</title>
-        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-        <style>
-            html, body {
-                background-color: #fff;
-            }
-
-            .flex-center {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-        </style>
+        <link href="{{ mix('/css/app.min.css') }}" rel="stylesheet">
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     </head>
-    <body>
+    <body class="bg-gradient-primary">
         @yield('content')
     </body>
 </html>

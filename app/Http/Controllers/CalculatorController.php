bb71d9bbd86a67fa497034db16050069902d7b27
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-17 21:08:47 -0400

    Added validator

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -6,7 +6,6 @@ use Illuminate\Routing\Controller;
 
 class CalculatorController extends Controller
 {
-
     public function index()
     {
         return view('calculator');
@@ -14,6 +13,12 @@ class CalculatorController extends Controller
 
     public function create(Request $request)
     {
+        $this->validate($request, [
+            'course_item' => 'required',
+            'worth_percent' => 'required',
+            'mark_percent' => 'required'
+        ]);
+
         print_r($request->course_item);
         print_r($request->worth_percent);
         print_r($request->mark_percent);

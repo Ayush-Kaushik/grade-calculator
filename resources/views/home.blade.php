d406de92761ddf241499549433479cb66d91aba5
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-18 15:24:16 -0400

    removed nav bar and fixed the input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,11 @@
 @extends('layout.main')
 @section('content')
-    <h1>Welcome to Grade Calculator!</h1>
-    <h2>Enter your code name or code</h2>
         <form action="{{ action('HomeController@create') }}" method="post" class="form-group">
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="course_name">Course Name</label>
                 <input type="text" name="course_name">
+                <button type="submit" class="btn btn-success">Add</button>
             </div>
-            <button type="submit" class="btn btn-submit">Add</button>
-            <button class="btn btn-warning">Clear</button>
         </form>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,13 +9,6 @@
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <nav class="navbar navbar-expand-sm bg-light">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="navbar-brand" href="#">Grade Calculator</a>
-              </li>
-            </ul>
-          </nav>
         @yield('content')
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     </body>

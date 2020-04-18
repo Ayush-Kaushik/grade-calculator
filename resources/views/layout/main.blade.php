03b83c6038762912195c6b2d520fa3df79bc63cd
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-18 15:15:39 -0400

    Added top navigation bar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,7 +9,7 @@
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        @yield('topnav')
+        @yield('layout.topnav')
         @yield('content')
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     </body>

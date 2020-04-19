c89a08285fce13d39262676acb99ea3b8fcb6213
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-19 19:34:38 -0400

    Fixed the logic when moving from home folder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,8 +15,10 @@ class HomeController extends Controller
     public function create(Request $request)
     {
         $request->session()->put('course_title', $request->course_name);
-        $request->session()->put('final_exam_stats', []);
-        session()->put('final_exam_worth', 0);
+        session()->put('final_exam_stats', []);
+        session()->put('final_exam_worth', 100);
+        session()->put('current_mark', 0);
+        session()->put('marks_list', []);
         return view('/calculator');
     }
 }

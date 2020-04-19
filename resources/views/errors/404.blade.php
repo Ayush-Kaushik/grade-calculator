2e66f40983391493bda573272317cfc18ee2f663
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-19 18:42:27 -0400

    Added error 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Grade Calculator</title>
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="text-center">
+                <div class="error mx-auto" data-text="404">404</div>
+                <p class="lead text-gray-800 mb-5">Page Not Found</p>
+                <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+            </div>
+        </div>
+    </body>
+</html>

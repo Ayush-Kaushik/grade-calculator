749ea2659db7bf1d725bf66d1cc5bdf0ec2cbcf0
OctcoShiv <sachdeva@mohawkcollege.ca>
2020-04-18 20:03:25 -0400

    fixing layout of the items on the calculator page

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -1,48 +1,51 @@
 @extends('layout.main')
 @section('content')
 <div class="container">
-    <div class="card shadow mb-4">
-        <form action="{{ action('CalculatorController@create') }}" method="post" class="form-group">
-            {{ csrf_field() }}
-            <div class="form-group">
-                <input placeholder="Course Item ..." type="text" class="form-control form-control-user" name="course_item" id="course_item">
+    <div class="row">
+        <div class="col-xl-8 col-lg-7">
+            <div class="card shadow mb-4">
+                <form action="{{ action('CalculatorController@create') }}" method="post" class="form-group">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <input placeholder="Course Item ..." type="text" class="form-control form-control-user" name="course_item" id="course_item">
+                    </div>
+        
+                    <div class="form-group">
+                        <input placeholder="Worth (in %)" class="form-control form-control-user" type="text" name="worth_percent" if="worth_percent">
+                    </div>
+        
+                    <div class="form-group">
+                        <input placeholder="Your Mark (in %)" class="form-control form-control-user" type="text" name="mark_percent" id="mark_percent">
+                    </div>
+        
+                    <button type="submit" class="btn btn-success btn-user btn-block">Add Entry</button>
+                </form>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
             </div>
-
-            <div class="form-group">
-                <input placeholder="Worth (in %)" class="form-control form-control-user" type="text" name="worth_percent" if="worth_percent">
-            </div>
-
-            <div class="form-group">
-                <input placeholder="Your Mark (in %)" class="form-control form-control-user" type="text" name="mark_percent" id="mark_percent">
-            </div>
-
-            <button type="submit" class="btn btn-success btn-user btn-block">Add Entry</button>
-        </form>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-    </div>
-    <div class="card shadow mb-2">
-        @if(Session::has('current_mark'))
+        </div>
+        <div class="col-xl-8 col-lg-7">
+            <div class="card shadow mb-2">
                 <p>Your current Mark is {{Session::get('current_mark')}}%</p>
-            @endif
-
-            @if(Session::has('final_exam_worth'))
                 <p>Final exam worth {{Session::get('final_exam_worth')}}%</p>
-            @endif
-
-            @if(Session::has('final_exam_stats'))
-                @foreach (Session::get('final_exam_stats') as $itemKey => $itemValue)
-                    <p>To finish with <b>{{$itemKey}}</b> required on final: {{$itemValue}}%</p>
-                @endforeach
-            @endif
+    
+                @if(Session::has('final_exam_stats'))
+                    @foreach (Session::get('final_exam_stats') as $itemKey => $itemValue)
+                        <p>To finish with <b>{{$itemKey}}</b> required on final: {{$itemValue}}%</p>
+                    @endforeach
+                @endif
+            </div>
+        </div>
     </div>
+
+
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h3 class="m-0 font-weight-bold text-primary">{{Session::get('course_title')}}</h3>
